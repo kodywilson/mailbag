73708280310778070657b26df8fbd01610250fb1
Kody Wilson <kody.wilson@oracle.com>
2020-02-28 16:10:27 -0800

    configure with json

--- mailbag.rb
+++ mailbag.rb
@@ -1,15 +1,18 @@
 require 'mail'  # ruby mail library. https://github.com/mikel/mail
 
+# Location of json configuration file
+conf    = JSON.parse(File.read('mailbag.json'))
+
 # configure delivery and retrieval methods
 Mail.defaults do
-  retriever_method :imap, { :address    => ENV["AUTOMATION_IMAPS"],
+  retriever_method :imap, { :address  => conf['email_host'],
                           :port       => 993,
-                          :user_name  => ENV["AUTOMATION_EMAIL"],
-                          :password   => ENV["AUTOMATION_PASSW"],
+                          :user_name  => conf['email_user'],
+                          :password   => conf['email_pass'],
                           :enable_ssl => true    }
 end
 
-# retrieve first 5 messages
+# retrieve first 2 messages
 emails = Mail.find(:what => :first, :count => 2)
 
 # or retrieve and delete from server first 5 emails
@@ -22,13 +25,13 @@ emails.each do |email|
 
     # email fields defined at https://github.com/mikel/mail/tree/master/lib/mail/fields
     puts "from     : " + email.from.to_s       #=> 'fromname@example.com'
-    puts "to       : " + email.to.to_s         #=> 'toname@example.com'
-    puts "cc       : " + email.cc.to_s         #=> 'ccname@example.com'
-    puts "bcc      : " + email.bcc.to_s        #=> 'bccname@example.com'    
+    #puts "to       : " + email.to.to_s         #=> 'toname@example.com'
+    #puts "cc       : " + email.cc.to_s         #=> 'ccname@example.com'
+    #puts "bcc      : " + email.bcc.to_s        #=> 'bccname@example.com'    
     
-    puts "subject  : " + email.subject         #=> "This is the subject"
-    puts "date     : " + email.date.to_s       #=> '26 Nov 2013 10:00:00 -0800'
-    puts "messageid: " + email.message_id      #=> '<ABCD1234.12345678@xxx.xxx>'
-    puts "body     : " + email.body.decoded    #=> 'This is the body of the email...    
+    #puts "subject  : " + email.subject         #=> "This is the subject"
+    #puts "date     : " + email.date.to_s       #=> '26 Nov 2013 10:00:00 -0800'
+    #puts "messageid: " + email.message_id      #=> '<ABCD1234.12345678@xxx.xxx>'
+    #puts "body     : " + email.body.decoded    #=> 'This is the body of the email...    
 
 end
