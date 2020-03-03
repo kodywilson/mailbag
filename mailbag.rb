157ed293d604643b392ad85b482fc8320d5a6242
Kody Wilson <kody.wilson@oracle.com>
2020-03-02 17:13:08 -0800

    can search for multiple types of emails now, add job types in the json

--- mailbag.rb
+++ mailbag.rb
@@ -8,6 +8,14 @@ require 'rest-client'
 # Location of json configuration file
 conf = JSON.parse(File.read('/run/secrets/mailbag.json'))
 
+# Generate values for some of the header fields
+def header_maker(hdr, kjob, e_date)
+  datey = Time.now.strftime('%Y/%m/%d')
+  hdr['file_name'] = kjob + '_' + datey + '.xlsx'
+  hdr['message_date'] = e_date.to_s
+  hdr
+end
+
 # configure delivery and retrieval methods
 Mail.defaults do
   retriever_method :imap, address: conf['email_host']['host_addr'],
@@ -17,33 +25,31 @@ Mail.defaults do
                           enable_ssl: true
 end
 
+# Iterate over each job defined in json config
 conf['mail_jobs'].each do |k, v|
-  # retrieve messages - search for sender
-  case v['search']
-  when 'sender'
-    emails = Mail.find(keys: ['FROM', v['po_from'], 'SENTON',
+  case v['search'] # Set keys for find according to search type
+  when 'sender'    # Email from a specific sender
+    emails = Mail.find(keys: ['FROM', v['frommy'], 'SENTON',
                               Time.now.strftime('%d-%b-%Y')])
-  when 'subject'
-    emails = Mail.find(keys: ['SUBJECT', v['sub_text']], count: 2, what: 'last')
+  when 'subject'   # Email containing a subject keyword
+    emails = Mail.find(keys: ['SUBJECT', v['sub_text'], 'SENTSINCE',
+                              (Time.now - 86_400).strftime('%d-%b-%Y'),
+                              'SENTBEFORE',
+                              (Time.now + 86_400).strftime('%d-%b-%Y')])
   else
-    'Error: Unknown search criteria'
+    next           # Move along if search type is not handled yet
   end
 
-  # loop thru all emails
-  emails.each do |email|
-    #  push attachments to database api endpoint
-    email.attachments.each do |attachment|
+  emails.each do |email| # Iterate over emails found meeting search criteria
+    email.attachments.each do |attachment| # Iterate over email attachments
       next unless attachment.content_type.start_with?('application/')
 
       begin
-        datey = Time.now.strftime('%Y/%m/%d')
-        headers = v['headers']
-        headers['file_name'] = k + '_' + datey + '.xlsx'
-        headers['message_date'] = email.date.to_s
+        headers = header_maker(v['headers'], k, email.date.to_s) # Set headers
         response = RestClient.post v['blob_url'], attachment.decoded, headers
-        puts response.code
+        puts response.code # Push attachment to api endpoint and show code
       rescue StandardError => e
-        puts "Unable to post #{filename} because #{e.message}"
+        puts "Unable to post because #{e.message}" # Something went wrong
       end
     end
   end
